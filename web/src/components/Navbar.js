5b2e809397d611a4830469b98befc3914e9df958
zuluroa@gmail.com <zuluroa@gmail.com>
2022-01-06 21:32:13 -0500

    Se cumple con las caracteristica #13 dando cobertura del 100% de las directrices

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const PublicNavbar = () => (
-  <nav>
+  <nav className="navbar navbar-expand-lg navbar-black bg-black">
+     <a class="navbar-brand" href="/">
+     <img src="./images/logoPet_project.png" width="50%" height="50%"/>
+     </a>
+    
     <section>
       <Link to="/">Home</Link>
       <Link to="/questions">Questions</Link>
